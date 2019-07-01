5f0aa1d6bd349c4c4668fd798030180eb457e812
Nemo <me@captnemo.in>
2019-07-02 02:06:08 +0530

    Initial Travis Setup

--- ci.sh
+++ ci.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-# we need dig
-sudo apt install dnsutils
 # Run the scan
 ./scan.sh
 
 cd website
 bundle install
 bundle exec jekyll build
+
+if ([ $TRAVIS_BRANCH == "travis" ] && [ $TRAVIS_PULL_REQUEST == "false" ]); then
+  cd _site
+  git init
+  git remote add origin "$GIT_REMOTE"
+  git add .
+  git commit -m "Deploy to GitHub Pages"
+  git push --force --quiet origin master:gh-pages > /dev/null 2>&1
+  echo 'Build successful, deployed to gh-pages.'
+else
+  echo "Build successful, but not deploying!"
+fi
