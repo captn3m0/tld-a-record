70e2a96dcec732b28ea8c4d97b4c96f34f055a6a
Nemo <me@captnemo.in>
2020-04-27 11:18:07 +0530

    Try to maintain history

--- scan.sh
+++ scan.sh
@@ -2,13 +2,13 @@
 
 # This script runs a scan for all available TLDs, and notes
 # down the TLDs that resolve to `website/template.md`
-# 
+#
 # It also puts some information about the IP Adress from where
 # the scan was run (might be relevant for DNS lookups) into
 # `website/_data/ip.json`. Structure is at ipapi.co
 
-for domain in $(grep -v '^#' tlds.txt); do 
-    RESULT=$(dig +time=1 +tries=1 +short "$domain" | head -c -1 | tr '\n' '@' | sed 's/@/`,`/g' | grep -v "connection timed out")
+for domain in $(grep -v '^#' tlds.txt); do
+    RESULT=$(dig +time=1 +tries=1 +short "$domain." | head -c -1 | tr '\n' '@' | sed 's/@/`,`/g' | grep -v "connection timed out")
     if [ ! -z "$RESULT" ]; then
         echo $domain
         DOMAIN_REAL="$domain"
